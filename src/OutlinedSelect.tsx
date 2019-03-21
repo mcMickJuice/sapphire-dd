9bd37cda1261134b35b61fa14c872c4fe38f9fb6
Mike Joyce <michael.joyce@target.com>
2019-03-20 19:51:49 -0500

    get dat label width working

--- src/OutlinedSelect.tsx
+++ src/OutlinedSelect.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReadDom from 'react-dom'
 import {
   Select,
   FormControl,
@@ -13,11 +14,22 @@ type Props = Pick<SelectProps, 'value' | 'onChange' | 'children'> & {
 }
 
 const OutlinedSelect = (props: Props) => {
+  const labelRef = React.useRef<InputLabel>(null)
+  const [labelWidth, updateLabelWidth] = React.useState(0)
+  React.useEffect(() => {
+    if (labelRef.current != null) {
+      const node = ReadDom.findDOMNode(labelRef.current) as HTMLElement
+      const width = node.offsetWidth
+      updateLabelWidth(width)
+    }
+  }, [])
   return (
     <FormControl fullWidth variant="outlined">
-      <InputLabel htmlFor={props.name}>{props.label}</InputLabel>
+      <InputLabel ref={labelRef} htmlFor={props.name}>
+        {props.label}
+      </InputLabel>
       <Select
-        input={<OutlinedInput labelWidth={200} name={props.name} />}
+        input={<OutlinedInput labelWidth={labelWidth} name={props.name} />}
         onChange={props.onChange}
         value={props.value}
       >
